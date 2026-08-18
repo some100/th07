Crystal1612/th07_vita

much much better naming
also corrects an egregious misnaming in gui (loadingSprite uses the same sprite idx as the sprite drawn in GameManager::DrawLoadingSprite)

src/th07/EffectManager.cpp
@@ -238,7 +238,7 @@ i32 EffectManager::UpdateBurst30Frames(Effect *effect)
 
 #pragma var_order(effect, i)
 // FUNCTION: TH07 0x0041adf0
-void EffectManager::DoSomethingWithEffects(Float3 *param_1)
+void EffectManager::ShiftEffectsAfterCameraTeleport(Float3 *shift)
 {
     i32 i;
     Effect *effect;
@@ -248,7 +248,7 @@ void EffectManager::DoSomethingWithEffects(Float3 *param_1)
     {
         if (effect->effectId == 20 || effect->effectId == 31)
         {
-            effect->basePosition += *param_1;
+            effect->basePosition += *shift;
         }
     }
 }
@@ -452,21 +452,21 @@ i32 EffectManager::InitWeatherFalling(Effect *effect)
 // FUNCTION: TH07 0x0041bec0
 i32 EffectManager::InitRandomDirWithSpeed(Effect *effect)
 {
-    f32 local_8;
+    f32 angle;
 
     // double intentionally used here, strangely
     if (effect->custom.x > -990.0)
     {
-        local_8 = utils::AddNormalizeAngle(effect->custom.x, 0.0f);
+        angle = utils::AddNormalizeAngle(effect->custom.x, 0.0f);
     }
     else
     {
-        local_8 = g_Rng.GetRandomFloatInRange(ZUN_2PI) - ZUN_PI;
+        angle = g_Rng.GetRandomFloatInRange(ZUN_2PI) - ZUN_PI;
     }
     effect->emitterPosition = effect->pos1;
     effect->emitterPosition.z = 0.0f;
-    effect->direction.x = cosf(local_8);
-    effect->direction.y = sinf(local_8);
+    effect->direction.x = cosf(angle);
+    effect->direction.y = sinf(angle);
     effect->direction.z = 0.0f;
     effect->direction *= g_Rng.GetRandomFloatInRange(1.5f) + 1.0f;
     return 0;

src/th07/EffectManager.hpp
@@ -72,7 +72,7 @@ struct EffectManager
     static i32 InitWeatherSlow(Effect *effect);
     static i32 InitWeatherFalling(Effect *effect);
 
-    static void DoSomethingWithEffects(Float3 *param_1);
+    static void ShiftEffectsAfterCameraTeleport(Float3 *shift);
     static void ModifyEffect1eAcceleration();
     static i32 UpdateNoOp(Effect *effect);
 

src/th07/Gui.cpp
@@ -63,7 +63,7 @@ void Gui::EndEnemySpellcard()
 void Gui::ClearActiveSprites()
 {
     this->impl->stageClearBg.activeSpriteIdx = -1;
-    this->impl->stageClearBonusTextVm.activeSpriteIdx = -1;
+    this->impl->loadingSprite.activeSpriteIdx = -1;
     this->impl->stageTransitionSnapshotVm.activeSpriteIdx = -1;
     this->impl->activeTransitionQuads = 0;
 }
@@ -696,16 +696,16 @@ ZunResult Gui::ActualAddedCallback()
 }
 
 // FUNCTION: TH07 0x00429935
-ZunResult Gui::LoadMsg(const char *param_1)
+ZunResult Gui::LoadMsg(const char *filename)
 {
     i32 i;
 
     FreeMsgFile();
-    this->impl->msg.msgFile = (MsgRawHeader *)FileSystem::OpenFile(param_1, 0);
+    this->impl->msg.msgFile = (MsgRawHeader *)FileSystem::OpenFile(filename, 0);
     if (!this->impl->msg.msgFile)
     {
         // STRING: TH07 0x00498108
-        g_GameErrorContext.Log("error : メッセージファイル %s が読み込めませんでした\r\n", param_1);
+        g_GameErrorContext.Log("error : メッセージファイル %s が読み込めませんでした\r\n", filename);
         return ZUN_ERROR;
     }
 
@@ -726,9 +726,9 @@ void Gui::FreeMsgFile()
 }
 
 // FUNCTION: TH07 0x00429a36
-void Gui::MsgRead(i32 param_1)
+void Gui::MsgRead(i32 msgIdx)
 {
-    this->impl->MsgRead(param_1);
+    this->impl->MsgRead(msgIdx);
 }
 
 // FUNCTION: TH07 0x00429a4f
@@ -1020,7 +1020,7 @@ ZunResult GuiImpl::RunMsg()
                     goto SKIP_TIME_INCREMENT;
                 }
 
-                g_AnmManager->InitializeAndSetActiveSprite(&this->stageClearBonusTextVm, 268);
+                g_AnmManager->InitializeAndSetActiveSprite(&this->loadingSprite, 268);
                 this->transitionToScoreScreen = 1;
                 this->msg.currentMsgIdx = -2;
             }
@@ -1797,10 +1797,10 @@ void Gui::DrawStageElements()
     {
         g_AnmManager->DrawNoRotation(&this->impl->stageClearBg);
         g_AnmManager->DrawNoRotation(&this->impl->stageTransitionSnapshotVm);
-        if (this->impl->stageClearBonusTextVm.activeSpriteIdx >= 0)
+        if (this->impl->loadingSprite.activeSpriteIdx >= 0)
         {
-            this->impl->stageClearBonusTextVm.pos = Float3(304.0f, 448.0f, 0.0f);
-            g_AnmManager->DrawNoRotation(&this->impl->stageClearBonusTextVm);
+            this->impl->loadingSprite.pos = Float3(304.0f, 448.0f, 0.0f);
+            g_AnmManager->DrawNoRotation(&this->impl->loadingSprite);
         }
     }
     if (this->impl->activeTransitionQuads != 0)

some100/th07

src/EffectManager.cpp
@@ -205,7 +205,7 @@ i32 EffectManager::UpdateBurst30Frames(Effect *effect)
     return 1;
 }
 
-void EffectManager::DoSomethingWithEffects(ZunVec3 *param_1)
+void EffectManager::ShiftEffectsAfterCameraTeleport(ZunVec3 *shift)
 {
     i32 i;
     Effect *effect;
@@ -215,7 +215,7 @@ void EffectManager::DoSomethingWithEffects(ZunVec3 *param_1)
     {
         if (effect->effectId == 20 || effect->effectId == 31)
         {
-            effect->basePosition += *param_1;
+            effect->basePosition += *shift;
         }
     }
 }
@@ -401,21 +401,21 @@ i32 EffectManager::InitWeatherFalling(Effect *effect)
 
 i32 EffectManager::InitRandomDirWithSpeed(Effect *effect)
 {
-    f32 local_8;
+    f32 angle;
 
     // double intentionally used here, strangely
     if (effect->custom.x > -990.0)
     {
-        local_8 = utils::AddNormalizeAngle(effect->custom.x, 0.0f);
+        angle = utils::AddNormalizeAngle(effect->custom.x, 0.0f);
     }
     else
     {
-        local_8 = g_Rng.GetRandomFloatInRange(ZUN_2PI) - ZUN_PI;
+        angle = g_Rng.GetRandomFloatInRange(ZUN_2PI) - ZUN_PI;
     }
     effect->emitterPosition = effect->pos1;
     effect->emitterPosition.z = 0.0f;
-    effect->direction.x = cosf(local_8);
-    effect->direction.y = sinf(local_8);
+    effect->direction.x = cosf(angle);
+    effect->direction.y = sinf(angle);
     effect->direction.z = 0.0f;
     effect->direction *= g_Rng.GetRandomFloatInRange(1.5f) + 1.0f;
     return 0;

src/EffectManager.hpp
@@ -73,7 +73,7 @@ struct EffectManager
     static i32 InitWeatherSlow(Effect *effect);
     static i32 InitWeatherFalling(Effect *effect);
 
-    static void DoSomethingWithEffects(ZunVec3 *param_1);
+    static void ShiftEffectsAfterCameraTeleport(ZunVec3 *shift);
     static void ModifyEffect1eAcceleration();
     static i32 UpdateNoOp(Effect *effect);
 

src/Gui.cpp
@@ -36,8 +36,7 @@ ChainElem g_GuiDrawChain;
 
 i32 Gui::IsStageFinished()
 {
-    return this->impl->stageClearTextVm.activeSpriteIdx >= 0 &&
-           this->impl->stageClearTextVm.isStopped;
+    return this->impl->stageClearBg.activeSpriteIdx >= 0 && this->impl->stageClearBg.isStopped;
 }
 
 void Gui::EndPlayerSpellcard()
@@ -55,8 +54,8 @@ void Gui::EndEnemySpellcard()
 
 void Gui::ClearActiveSprites()
 {
-    this->impl->stageClearTextVm.activeSpriteIdx = -1;
-    this->impl->stageClearBonusTextVm.activeSpriteIdx = -1;
+    this->impl->stageClearBg.activeSpriteIdx = -1;
+    this->impl->loadingSprite.activeSpriteIdx = -1;
     this->impl->stageTransitionSnapshotVm.activeSpriteIdx = -1;
     this->impl->activeTransitionQuads = 0;
 }
@@ -646,13 +645,13 @@ ZunResult Gui::ActualAddedCallback()
     return ZUN_SUCCESS;
 }
 
-ZunResult Gui::LoadMsg(const char *param_1)
+ZunResult Gui::LoadMsg(const char *filename)
 {
     FreeMsgFile();
-    this->impl->msg.msgFile = (MsgRawHeader *)FileSystem::OpenFile(param_1, 0);
+    this->impl->msg.msgFile = (MsgRawHeader *)FileSystem::OpenFile(filename, 0);
     if (!this->impl->msg.msgFile)
     {
-        g_GameErrorContext.Log("error : メッセージファイル %s が読み込めませんでした\n", param_1);
+        g_GameErrorContext.Log("error : メッセージファイル %s が読み込めませんでした\n", filename);
         return ZUN_ERROR;
     }
 
@@ -667,9 +666,9 @@ void Gui::FreeMsgFile()
     SAFE_FREE(this->impl->msg.msgFile);
 }
 
-void Gui::MsgRead(i32 param_1)
+void Gui::MsgRead(i32 msgIdx)
 {
-    this->impl->MsgRead(param_1);
+    this->impl->MsgRead(msgIdx);
 }
 
 void GuiImpl::MsgRead(i32 msgIdx)
@@ -885,7 +884,7 @@ ZunResult GuiImpl::RunMsg()
             this->finishedStage = 1;
             if (g_GameManager.currentStage < 6)
             {
-                g_AnmManager->SetAnmIdxAndExecuteScript(&this->stageClearTextVm, 1566);
+                g_AnmManager->SetAnmIdxAndExecuteScript(&this->stageClearBg, 1566);
                 g_AnmManager->SetAnmIdxAndExecuteScript(&this->stageTransitionSnapshotVm, 1829);
                 g_AnmManager->CreateScreenshotTexture(
                     this->stageTransitionSnapshotVm.sprite->startPixelInclusive.x,
@@ -927,7 +926,7 @@ ZunResult GuiImpl::RunMsg()
                     goto SKIP_TIME_INCREMENT;
                 }
 
-                g_AnmManager->InitializeAndSetActiveSprite(&this->stageClearBonusTextVm, 268);
+                g_AnmManager->InitializeAndSetActiveSprite(&this->loadingSprite, 268);
                 this->transitionToScoreScreen = 1;
                 this->msg.currentMsgIdx = -2;
             }
@@ -1171,11 +1170,11 @@ void Gui::UpdateGui()
     g_AnmManager->ExecuteScript(&this->impl->bombSpellcardNameBg);
     g_AnmManager->ExecuteScript(&this->impl->enemySpellcardNameBg);
     g_AnmManager->ExecuteScript(&this->impl->spellcardBonusIndicator);
-    if (this->impl->stageClearTextVm.activeSpriteIdx >= 0)
+    if (this->impl->stageClearBg.activeSpriteIdx >= 0)
     {
-        if (g_AnmManager->ExecuteScript(&this->impl->stageClearTextVm))
+        if (g_AnmManager->ExecuteScript(&this->impl->stageClearBg))
         {
-            this->impl->stageClearTextVm.activeSpriteIdx = -1;
+            this->impl->stageClearBg.activeSpriteIdx = -1;
         }
         if (g_AnmManager->ExecuteScript(&this->impl->stageTransitionSnapshotVm) != 0)
         {
@@ -1633,14 +1632,14 @@ void Gui::DrawStageElements()
         this->impl->captureBonusVm.sprite = g_AnmManager->GetSprite(digit % 10 + 132);
         g_AnmManager->DrawNoRotation(&this->impl->captureBonusVm);
     }
-    if (this->impl->stageClearTextVm.activeSpriteIdx >= 0)
+    if (this->impl->stageClearBg.activeSpriteIdx >= 0)
     {
-        g_AnmManager->DrawInterpNoRotation(&this->impl->stageClearTextVm);
+        g_AnmManager->DrawInterpNoRotation(&this->impl->stageClearBg);
         g_AnmManager->DrawInterpNoRotation(&this->impl->stageTransitionSnapshotVm);
-        if (this->impl->stageClearBonusTextVm.activeSpriteIdx >= 0)
+        if (this->impl->loadingSprite.activeSpriteIdx >= 0)
         {
-            this->impl->stageClearBonusTextVm.pos = ZunVec3(304.0f, 448.0f, 0.0f);
-            g_AnmManager->DrawInterpNoRotation(&this->impl->stageClearBonusTextVm);
+            this->impl->loadingSprite.pos = ZunVec3(304.0f, 448.0f, 0.0f);
+            g_AnmManager->DrawInterpNoRotation(&this->impl->loadingSprite);
         }
     }
     if (this->impl->activeTransitionQuads != 0)