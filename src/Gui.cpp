Crystal1612/th07_vita

move bombeffects into screeneffect, some renaming, more de-hardcoding of anm offsets
bombeffects, while primarily used in bombdata, was also used outside of bombdata hence why its just screeneffect now.

src/th07/Gui.cpp
@@ -1022,7 +1022,8 @@ ZunResult GuiImpl::RunMsg()
             g_Supervisor.FadeOutMusic(4.0f);
             break;
         case MSG_FADE_IN_EFFECT:
-            BombEffects::RegisterChain(4, 0x192, 0xffffff, 0, 0);
+            ScreenEffect::RegisterChain(
+                SCREEN_EFFECT_FADE_IN_FULLSCREEN, 0x192, 0xffffff, 0, 0);
             g_Supervisor.renderSkipFrames = 0x192;
             break;
         case MSG_NEXT_LEVEL:
@@ -1456,8 +1457,8 @@ void Gui::DrawGameScene()
     g_AnmManager->Flush();
     g_Supervisor.viewport.X = 0;
     g_Supervisor.viewport.Y = 0;
-    g_Supervisor.viewport.Width = 640;
-    g_Supervisor.viewport.Height = 480;
+    g_Supervisor.viewport.Width = GAME_WINDOW_WIDTH;
+    g_Supervisor.viewport.Height = GAME_WINDOW_HEIGHT;
     g_Supervisor.d3dDevice->SetViewport(&g_Supervisor.viewport);
     vm = &this->impl->vms0[12];
     if (g_Supervisor.cfg.redrawEveryFrame ||
@@ -1912,7 +1913,7 @@ void Gui::DrawStageElements()
         if (secondsRemaining < 10 && this->lastSpellcardSecondsRemaining !=
                                          this->spellcardSecondsRemaining)
         {
-            g_SoundPlayer.PlaySoundByIdx(SOUND_29, 0);
+            g_SoundPlayer.PlaySoundByIdx(SOUND_LOW_SPELL_TIME_WARNING, 0);
         }
         AsciiManager::AddFormatText(&g_AsciiManager, &timerPos, "%.2d", secondsRemaining);
         g_AsciiManager.color = 0xffffffff;

src/th07/ScreenEffect.hpp
@@ -6,6 +6,15 @@
 #include "ZunTimer.hpp"
 #include "inttypes.hpp"
 
+enum ScreenEffectsType
+{
+    SCREEN_EFFECT_FADE_OUT,
+    SCREEN_EFFECT_SHAKE,
+    SCREEN_EFFECT_FADE_IN_PLAY_AREA,
+    SCREEN_EFFECT_PULSE,
+    SCREEN_EFFECT_FADE_IN_FULLSCREEN,
+};
+
 struct ZunRect
 {
     f32 left;
@@ -14,29 +23,26 @@ struct ZunRect
     f32 bottom;
 };
 
-namespace ScreenEffect
+struct ScreenEffect
 {
-void Clear(D3DCOLOR color);
-void DrawSquare(ZunRect *rect, D3DCOLOR color);
-void DrawColoredQuad(ZunRect *rect, D3DCOLOR param_2, D3DCOLOR param_3,
-                     D3DCOLOR param_4, D3DCOLOR param_5);
-void SetViewport(D3DCOLOR color);
-} // namespace ScreenEffect
+    static ScreenEffect *RegisterChain(i32 type, i32 duration, u32 arg1, u32 arg2,
+                                       u32 arg3);
 
-struct BombEffects
-{
-    static BombEffects *RegisterChain(i32 type, i32 duration, u32 arg1, u32 arg2,
-                                      u32 arg3);
+    static ZunResult AddedCallback(ScreenEffect *arg);
+    static ZunResult DeletedCallback(ScreenEffect *arg);
+    static u32 OnUpdateFadeIn(ScreenEffect *arg);
+    static u32 OnUpdateFadeOut(ScreenEffect *arg);
+    static u32 OnUpdatePulse(ScreenEffect *arg);
+    static u32 OnUpdateScreenShake(ScreenEffect *arg);
+    static u32 OnDrawFullScreenColor(ScreenEffect *arg);
+    static u32 OnDrawPlayAreaColor(ScreenEffect *arg);
+    static u32 OnDrawPlayAreaPulseColor(ScreenEffect *arg);
 
-    static ZunResult AddedCallback(BombEffects *arg);
-    static ZunResult DeletedCallback(BombEffects *arg);
-    static u32 OnUpdateFadeIn(BombEffects *arg);
-    static u32 OnUpdateFadeOut(BombEffects *arg);
-    static u32 OnUpdatePulse(BombEffects *arg);
-    static u32 OnUpdateScreenShake(BombEffects *arg);
-    static u32 OnDrawFullScreenColor(BombEffects *arg);
-    static u32 OnDrawPlayAreaColor(BombEffects *arg);
-    static u32 OnDrawPlayAreaPulseColor(BombEffects *arg);
+    static void Clear(D3DCOLOR color);
+    static void DrawSquare(ZunRect *rect, D3DCOLOR color);
+    static void DrawColoredQuad(ZunRect *rect, D3DCOLOR param_2, D3DCOLOR param_3,
+                                D3DCOLOR param_4, D3DCOLOR param_5);
+    static void SetViewport(D3DCOLOR color);
 
     i32 type;
     ChainElem *calcChain;
@@ -47,4 +53,4 @@ struct BombEffects
     u32 args[3];
     ZunTimer timer;
 };
-C_ASSERT(sizeof(BombEffects) == 0x30);
+C_ASSERT(sizeof(ScreenEffect) == 0x30);

some100/th07

src/Gui.cpp
@@ -950,7 +950,8 @@ ZunResult GuiImpl::RunMsg()
             g_Supervisor.FadeOutMusic(4.0f);
             break;
         case MSG_FADE_IN_EFFECT:
-            BombEffects::RegisterChain(4, 0x192, 0xffffff, 0, 0);
+            ScreenEffect::RegisterChain(
+                SCREEN_EFFECT_FADE_IN_FULLSCREEN, 0x192, 0xffffff, 0, 0);
             g_Supervisor.renderSkipFrames = 0x192;
             break;
         case MSG_NEXT_LEVEL:
@@ -1366,8 +1367,8 @@ void Gui::DrawGameScene()
     g_AnmManager->Flush();
     g_Supervisor.viewport.x = 0;
     g_Supervisor.viewport.y = 0;
-    g_Supervisor.viewport.width = 640;
-    g_Supervisor.viewport.height = 480;
+    g_Supervisor.viewport.width = GAME_WINDOW_WIDTH;
+    g_Supervisor.viewport.height = GAME_WINDOW_HEIGHT;
     g_Supervisor.gfxDevice->SetViewport(g_Supervisor.viewport);
     vm = &this->impl->vms0[12];
     if (g_Supervisor.cfg.redrawEveryFrame || vm->currentInstruction ||
@@ -1788,7 +1789,7 @@ void Gui::DrawStageElements()
         if (secondsRemaining < 10 &&
             this->lastSpellcardSecondsRemaining != this->spellcardSecondsRemaining)
         {
-            g_SoundPlayer.PlaySoundByIdx(SOUND_29, 0);
+            g_SoundPlayer.PlaySoundByIdx(SOUND_LOW_SPELL_TIME_WARNING, 0);
         }
         AsciiManager::AddFormatText(&g_AsciiManager, &timerPos, "%.2d", secondsRemaining);
         g_AsciiManager.color = 0xffffffff;

src/ScreenEffect.hpp
@@ -4,6 +4,15 @@
 #include "ZunTimer.hpp"
 #include "inttypes.hpp"
 
+enum ScreenEffectsType
+{
+    SCREEN_EFFECT_FADE_OUT,
+    SCREEN_EFFECT_SHAKE,
+    SCREEN_EFFECT_FADE_IN_PLAY_AREA,
+    SCREEN_EFFECT_PULSE,
+    SCREEN_EFFECT_FADE_IN_FULLSCREEN,
+};
+
 struct ZunRect
 {
     f32 left;
@@ -12,28 +21,26 @@ struct ZunRect
     f32 bottom;
 };
 
-namespace ScreenEffect
+struct ScreenEffect
 {
-void Clear(ZunColor color);
-void DrawSquare(ZunRect *rect, u32 color);
-void DrawColoredQuad(ZunRect *rect, u32 param_2, u32 param_3, u32 param_4,
-                     u32 param_5);
-void SetViewport(u32 color);
-} // namespace ScreenEffect
+    static ScreenEffect *RegisterChain(i32 type, i32 duration, u32 arg1, u32 arg2,
+                                       u32 arg3);
 
-struct BombEffects
-{
-    static BombEffects *RegisterChain(i32 type, i32 duration, u32 arg1, u32 arg2, u32 arg3);
+    static ZunResult AddedCallback(ScreenEffect *arg);
+    static ZunResult DeletedCallback(ScreenEffect *arg);
+    static u32 OnUpdateFadeIn(ScreenEffect *arg);
+    static u32 OnUpdateFadeOut(ScreenEffect *arg);
+    static u32 OnUpdatePulse(ScreenEffect *arg);
+    static u32 OnUpdateScreenShake(ScreenEffect *arg);
+    static u32 OnDrawFullScreenColor(ScreenEffect *arg);
+    static u32 OnDrawPlayAreaColor(ScreenEffect *arg);
+    static u32 OnDrawPlayAreaPulseColor(ScreenEffect *arg);
 
-    static ZunResult AddedCallback(BombEffects *arg);
-    static ZunResult DeletedCallback(BombEffects *arg);
-    static u32 OnUpdateFadeIn(BombEffects *arg);
-    static u32 OnUpdateFadeOut(BombEffects *arg);
-    static u32 OnUpdatePulse(BombEffects *arg);
-    static u32 OnUpdateScreenShake(BombEffects *arg);
-    static u32 OnDrawFullScreenColor(BombEffects *arg);
-    static u32 OnDrawPlayAreaColor(BombEffects *arg);
-    static u32 OnDrawPlayAreaPulseColor(BombEffects *arg);
+    static void Clear(ZunColor color);
+    static void DrawSquare(ZunRect *rect, u32 color);
+    static void DrawColoredQuad(ZunRect *rect, u32 param_2, u32 param_3,
+                                u32 param_4, u32 param_5);
+    static void SetViewport(u32 color);
 
     i32 type;
     ChainElem *calcChain;